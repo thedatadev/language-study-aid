81e41c6046948080fc677a289b1c388481ba70f6
thedatadev <thedatadev@gmail.com>
2019-04-30 20:58:05 +1000

    recommender setup

---
+++ languages/japanese/recommender/recommender.py
@@ -0,0 +1,13 @@
+def recommend(doc_id):
+
+    # 1. Retrieve embedding for doc_id
+
+    # 2. Retrieve embeddings for all doc_ids minus that of doc_id
+
+    # 3. Compute cosine similarity between doc_id and the rest
+
+    # 4. Sort the results based on cosine similarity
+
+    # 5. Return top k results
+    
+    pass
